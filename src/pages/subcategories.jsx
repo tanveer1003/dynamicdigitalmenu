f5873668e1e9ff5aef6d952f28128c390ce0e067
tanveer1003 <126807108+tanveer1003@users.noreply.github.com>
2025-05-20 21:28:57 +0500

    Update subcategories.jsx

--- src/pages/subcategories.jsx
+++ src/pages/subcategories.jsx
@@ -386,7 +386,7 @@ function SubcategoriesPage({ language }) {
                                                 </span>
 
                                                 <div class="card-body">
-                                                    <div className="product-row">
+                                                    <div className={`product-row ${language === "he" ? "flex-row-reverse" : ""}`}>
                                                         <h5 className="product-title"> {language === "he"
                                                             ? productTranslations[language][selectedProduct.title].title || selectedProduct.title
                                                             : selectedProduct.title} </h5>
